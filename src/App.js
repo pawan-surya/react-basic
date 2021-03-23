c3eb3a09bb760e1783090bf5e017ddba4b2c687f
Spixs <info@iizii.in>
2021-03-23 18:35:45 +0530

    create new project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './my_logo.png';
+import "./app/App.css"
+import { BrowserRouter as Router,Route,Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is My Frist Learing project just check it out below.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="http://localhost:8080/" target="_blank"  rel="noopener noreferrer">My Project</a>  
       </header>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Header = (props) => {
+
+    return (
+        <h1>Header</h1>
+    )
+}
